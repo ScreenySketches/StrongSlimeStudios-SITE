87e0d5be09fb1232f5e90e9de6315b22c02cf747
Sean Dotson <seand0602@gmail.com>
2023-01-01 04:48:23 -0800

    Polishing Pages, Adding JS
    Polished up the 404 page
    
    Main Logo on Home page now properly set to "cover" and changes opacity on scroll
    
    All current pages have the Nav Menu

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,15 @@
+$(window).scroll(function() {
+  var scrollTop = $(this).scrollTop();
 
+      $('.hero.is-strongslime').css({
+      opacity: function() {
+          var elementHeight = $(this).height(),
+          opacity = ((elementHeight - scrollTop) / elementHeight - 0.1);
+          return opacity;
+      }
+  });
+});
 
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll(nav, a).forEach
 
