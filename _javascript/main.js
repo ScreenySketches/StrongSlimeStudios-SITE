4ab51025b32473c83d8b3d6e8575586cd82ec4a6
Sean Dotson <seand0602@gmail.com>
2023-01-01 05:37:15 -0800

    Update main.js

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,15 +1,23 @@
-$(window).scroll(function() {
-  var scrollTop = $(this).scrollTop();
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-      $('.hero.is-strongslime').css({
-      opacity: function() {
-          var elementHeight = $(this).height(),
-          opacity = ((elementHeight - scrollTop) / elementHeight - 0.1);
-          return opacity;
-      }
+
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+  
+        $('.hero.is-strongslime').css({
+        opacity: function() {
+            var elementHeight = $(this).height(),
+            opacity = ((elementHeight - scrollTop) / elementHeight - 0.1);
+            return opacity;
+        }
+    });
   });
+  
+  const activePage = window.location.pathname;
+  const navLinks = document.querySelectorAll(nav, a).forEach
+
+  
+  
 });
 
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll(nav, a).forEach
 
