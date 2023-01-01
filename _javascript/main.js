29d4062ae26293e730010662f15a718ada4b4b56
Sean Dotson <seand0602@gmail.com>
2023-01-01 13:42:54 -0800

    testing out alternative main.js in lib

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,23 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
 
-
-  $(window).on('scroll', function() {
-    var scrollTop = $(this).scrollTop();
-  
-        $('.hero.is-strongslime').css({
-        opacity: function() {
-            var elementHeight = $(this).height(),
-            opacity = ((elementHeight - scrollTop) / elementHeight - 0.15);
-            return opacity;
-        }
-    });
-  });
-  
-  const activePage = window.location.pathname;
-  const navLinks = document.querySelectorAll(nav, a).forEach
-
-
-  
-});
 
 
